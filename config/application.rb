1cae375f73b7b4a7685631a5a82ba16e9e61119f
Matt Rooney <matthewjrooney@gmail.com>
2016-01-11 16:40:01 -0700

    add users controller

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module LiveList
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    RSpotify::authenticate("spotify_api_key", "spotify_api_secret")
+    RSpotify::authenticate(ENV["spotify_api_key"], ENV["spotify_api_secret"])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+  get '/auth/spotify', as: :login
   get '/auth/spotify/callback', to: 'users#spotify'
   resources :playlists, only: [:new]
 end
