65f6814e49985abf17695bcdfa7695c331b47ce4
Matt Rooney <matthewjrooney@gmail.com>
2016-01-12 09:09:52 -0700

    update views and controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    if user = RSpotify::User.new(request.env['omniauth.auth'])
+    if user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
     end
-    redirect_to current_user
+    redirect_to user_path(user)
   end
 
 end
