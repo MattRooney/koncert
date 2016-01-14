9129a96762f7efd1fcb2721c4f83083fcd561468
Matt Rooney <matthewjrooney@gmail.com>
2016-01-14 13:50:26 -0700

    update playlist controller show

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -16,15 +16,15 @@ class PlaylistsController < ApplicationController
         RSpotify::Artist.search(artist).first.top_tracks(:us).first
     end
     spotify_tracks = tracks.delete_if { |track| track == nil }
-    playlist = spotify_service.create_playlist(params[:playlist][:location])
+    binding.pry
+    playlist = spotify_service.create_playlist(params[:playlist][:location]+" #{Time.now.strftime("%m/%d/%Y")}")
     found_playlist = RSpotify::Playlist.find(current_user.spotify_id, playlist.id)
     found_playlist.add_tracks!(spotify_tracks)
     redirect_to "/playlists/1"
   end
 
   def show
-    playlist = spotify_service.playlists.first.uri
-    @embed_code = ActiveSupport::SafeBuffer.new(%Q{<iframe src="https://embed.spotify.com/?uri=#{playlist}" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>})
+    @playlist = spotify_service.playlists.first
   end
 
 end
