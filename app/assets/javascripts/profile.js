fc8d44699154b0e8c848863436e7b518e2fc7e18
Matt Rooney <matthewjrooney@gmail.com>
2016-01-19 20:59:10 -0700

    wip - playlist create ajax call

---
+++ app/assets/javascripts/profile.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//     $('.play-button').click();
+// })
+//  
+// var x = document.getElementById("demo");
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else {
+//         x.innerHTML = "Geolocation is not supported by this browser.";
+//     }
+// }
+// function showPosition(position) {
+//     x.innerHTML = "Latitude: " + position.coords.latitude +
+//     "<br>Longitude: " + position.coords.longitude;
+// }
---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,29 @@
+class PlaylistsController < ApplicationController
+
+  def index
+    @playlists = spotify_service.playlists
+  end
+
+  def new
+  end
+
+  def create
+    # events = bandsintown_service.events(params[:playlist][:location])
+    # artists = bandsintown_service.artists(events)
+    # spotify_artists = spotify_service.clean_artists(artists)
+    # top_tracks = spotify_service.top_tracks(spotify_artists)
+    # spotify_tracks = spotify_service.clean_tracks(top_tracks)
+    # spotify_playlist = spotify_service.create_playlist("Koncert " + params[:playlist][:location] + " #{Time.now.strftime("%m/%d/%Y")}")
+    # spotify_service.add_tracks(spotify_playlist, spotify_tracks)
+    # redirect_to playlist_path(spotify_playlist.id)
+  end
+
+  def show
+    # @playlist = spotify_service.find_playlist(current_user.spotify_id, params[:id])
+    # @events = bandsintown_service.playlist_events(@playlist.name.split[1, 2].join(" "))
+  end
+
+  def destroy
+  end
+
+end
