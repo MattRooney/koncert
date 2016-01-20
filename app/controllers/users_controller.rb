4f2a9638b75ad1f67b42d1d7f31a800ced754005
Matt Rooney <matthewjrooney@gmail.com>
2016-01-20 12:57:45 -0700

    implement spotify profile and playlists ajax call - update spotify js to fetchSpotifyPlaylists & fetchProfile - replaces playlist#index and user#show methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def current
+    render json: current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth_info)
-    where(spotify_id: auth_info[:info][:id]).first_or_create do |new_user|
-      new_user.provider           = auth_info.provider
-      new_user.spotify_id         = auth_info.info.id
-      new_user.name               = auth_info.info.display_name
-      new_user.email              = auth_info.info.email
-      new_user.oauth_token        = auth_info.credentials.token
-      new_user.oauth_token_secret = auth_info.credentials.refresh_token
-    end
+    user = find_or_create_by(spotify_id: auth_info[:info][:id])
+      user.update_attributes(
+        provider: auth_info.provider,
+        spotify_id: auth_info.info.id,
+        name: auth_info.info.display_name,
+        email: auth_info.info.email,
+        oauth_token: auth_info.credentials.token,
+        oauth_token_secret: auth_info.credentials.refresh_token
+        )
+        user
   end
 end
